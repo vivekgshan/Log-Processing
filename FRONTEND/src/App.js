8c81162ad58fd2bcce546ca97fd4cc4b9e375bc3
ramakrishna1918 <95967073+ramakrishna1918@users.noreply.github.com>
2025-09-04 15:35:42 +0530

    Added table dashboard updated styling

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Home from './screens/Home';
-import TableScreen from './screens/TableScreen';
-import GraphScreen from './screens/Graphscreen';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Home from "./screens/Home";
+import TableScreen from "./screens/TableScreen";
+import GraphScreen from "./screens/Graphscreen";
+import Header from "./components/Header";
+import data from "./mock/logs.json";
 
-function App() {
+const App = () => {
+
+  const [logs, setLogs] = useState(data.logs);
+  const [counts, setCounts] = useState(data.totalCounts);
+  const [lastUpdated, setLastUpdated] = useState(null);
+
+  const handleRefresh = () => {
+    setLogs(data.logs);
+    setCounts(data.totalCounts);
+    setLastUpdated(new Date().toLocaleTimeString());
+  };
+
   return (
     <Router>
-      <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex" }}>
         <Sidebar />
-        <div style={{ marginLeft: '195px', padding: '20px', flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tableView" element={<TableScreen />} />
-            <Route path="/graphView" element={<GraphScreen />} />
-          </Routes>
+        <div className="main-content">
+
+          <div style={{ marginLeft: "195px", flex: 1 }}>
+            <Header onRefresh={handleRefresh} lastUpdated={lastUpdated} />
+            <Routes>
+              <Route path="/" element={<Home logs={logs} counts={counts} />} />
+              <Route path="/tableView" element={<TableScreen logs={logs} />} />
+              <Route path="/graphView" element={<GraphScreen logs={logs} />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
