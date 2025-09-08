890cdc94fa18145388c284cfd485260cfedea1bf
santrajrakesh <rakesh.santraj4@gmail.com>
2025-09-08 18:24:21 +0530

    cleaning code base

--- BACKEND/Logmonitoringapp/logWarnPersistor/src/main/java/com/logwarnpersistor/repository/LogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logwarnpersistor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.logwarnpersistor.model.LogEntity;
-
-public interface LogRepository extends JpaRepository<LogEntity, Long> {
-}
