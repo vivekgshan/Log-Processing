ce6446b115acdd5b18ce8d1aa8a177c955f2fbad
santrajrakesh <rakesh.santraj4@gmail.com>
2025-09-11 07:59:19 +0530

    Time stamp issue

--- BACKEND/logdebugpersistor/src/main/java/com/logdebugpersistor/controller/DebugController.java
+++ BACKEND/logdebugpersistor/src/main/java/com/logdebugpersistor/controller/DebugController.java
@@ -1,5 +1,7 @@
 package com.logdebugpersistor.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.logdebugpersistor.DTO.LogEntry;
 import com.logdebugpersistor.model.LogEntity;
 import com.logdebugpersistor.repository.LogRepository;
 
@@ -22,9 +25,16 @@ public class DebugController {
 		this.logRepository = logRepository;
 	}
 
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 	@PostMapping
-	public ResponseEntity<String> saveLog(@RequestBody LogEntity log) {
-		logRepository.save(log);
+	public ResponseEntity<String> saveLog(@RequestBody LogEntry log) {
+		LogEntity le= new LogEntity();
+		le.setLogType(log.getLogType());
+		le.setMessage(log.getMessage());
+		le.setTimestamp(LocalDateTime.parse(log.getTimestamp(), FORMATTER));
+		logRepository.save(le);
+
 		System.out.println("✅ Saved DEBUG log: " + log.getMessage());
 		return ResponseEntity.ok("Debug log saved successfully");
 	}
